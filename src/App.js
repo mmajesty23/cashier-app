90d86e51c319633b874aa3aa4943a01fda2915c1
mmajesty23 <muhammadmajesty23@gmail.com>
2025-01-06 12:47:51 +0700

    Display menu for each category and change the category by the category choosen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,78 @@
 import React, { Component } from "react";
 import NavbarComponent from "./components/NavbarComponent.js";
 import ListCategoryComponent from "./components/ListCategoryComponent.js";
-import ListProductComponent from "./components/ListProductComponent.js";
 import OrderSummaryComponent from "./components/OrderSummaryComponent.js";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "./utils/constant.js";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menus: [],
+      choosenCategory: "Makanan",
+    };
+  }
+
+  componentDidMount() {
+    try {
+      axios
+        .get(`${API_URL}products?category.nama=${this.state.choosenCategory}`)
+        .then((res) => this.setState({ menus: res.data }));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  changeChoosenCategory = (categoryValue) => {
+    this.setState({ choosenCategory: categoryValue, menus: [] });
+    try {
+      axios
+        .get(`${API_URL}products?category.nama=${categoryValue}`)
+        .then((res) => this.setState({ menus: res.data }));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   render() {
+    const { menus, choosenCategory } = this.state;
+
     return (
       <>
         <NavbarComponent />
         <Container fluid className="mt-3">
           <Row>
-            <ListCategoryComponent />
-            <ListProductComponent />
+            <ListCategoryComponent
+              changeChoosenCategory={this.changeChoosenCategory}
+              choosenCategory={choosenCategory}
+            />
+            <Col>
+              <h3>List Menu</h3>
+              <hr />
+              <Row>
+                {menus &&
+                  menus.map((menu) => {
+                    return (
+                      <Col md={4} className="mb-3" key={menu.id}>
+                        <Card className="shadow">
+                          <Card.Img
+                            variant="top"
+                            src={`/assets/images/${menu.category.nama.toLowerCase()}/${
+                              menu.gambar
+                            }`}
+                          />
+                          <Card.Body>
+                            <Card.Title>{menu.nama}</Card.Title>
+                            <Card.Text>{menu.harga}</Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    );
+                  })}
+              </Row>
+            </Col>
             <OrderSummaryComponent />
           </Row>
         </Container>
--- src/components/ListCategoryComponent.js
+++ src/components/ListCategoryComponent.js
@@ -1,23 +1,8 @@
 import React, { Component } from "react";
+import { Col } from "react-bootstrap";
 import axios from "axios";
 import { API_URL } from "../utils/constant";
-import { Col, Row, ListGroup } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMartiniGlass,
-  faCheese,
-  faUtensils,
-} from "@fortawesome/free-solid-svg-icons";
-
-const Icon = ({ iconCategoryDetails }) => {
-  if (iconCategoryDetails === "Makanan")
-    return <FontAwesomeIcon icon={faUtensils} />;
-  if (iconCategoryDetails === "Minuman")
-    return <FontAwesomeIcon icon={faMartiniGlass} />;
-  if (iconCategoryDetails === "Cemilan")
-    return <FontAwesomeIcon icon={faCheese} />;
-  return;
-};
+import { ListGroup } from "react-bootstrap";
 
 export default class ListCategoryComponent extends Component {
   constructor(props) {
@@ -28,40 +13,46 @@ export default class ListCategoryComponent extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get(`${API_URL}categories`)
-      .then((res) => this.setState({ categories: res.data }));
+    try {
+      axios
+        .get(`${API_URL}categories`)
+        .then((res) => this.setState({ categories: res.data }));
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   render() {
     const { categories } = this.state;
+    const { changeChoosenCategory, choosenCategory } = this.props;
 
     return (
       <>
         <Col md={3}>
           <h3>List Category</h3>
           <hr />
-          <Row>
+
+          <ListGroup>
             {categories &&
               categories.map((category) => {
                 return (
-                  <>
-                    <ListGroup className="mb-2 cursor-pointer">
-                      <ListGroup.Item className="d-flex align-items-center gap-2">
-                        <Icon iconCategoryDetails={category.nama} />
-                        {category.nama}
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </>
+                  <ListGroup.Item
+                    key={category.id}
+                    className={
+                      choosenCategory === category.nama
+                        ? "category--active"
+                        : ""
+                    }
+                    onClick={() => changeChoosenCategory(category.nama)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {category.nama}
+                  </ListGroup.Item>
                 );
               })}
-          </Row>
+          </ListGroup>
         </Col>
       </>
     );
   }
 }
-
-// 1. Create a state to hold data from rest API (v)
-// 2. fetch data (v)
-// 3. display data
